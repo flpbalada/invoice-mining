e476be77d25f060f279e36729d463752125434e4
Filip Balada <flpbalada@gmail.com>
2025-06-19 13:53:13 +0200

    fix: Refactor InvoiceMiningUploadFilesForm for improved state management and user feedback

--- src/features/invoice-mining/components/invoice-mining-upload-files-form.tsx
+++ src/features/invoice-mining/components/invoice-mining-upload-files-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useActionState } from 'react'
+import { ReactNode, useActionState, useState } from 'react'
 import Link from 'next/link'
 import { FaCheck } from 'react-icons/fa6'
 import { invoiceMiningUploadFilesFormAction } from '../actions/invoice-mining-upload-files-form-action'
@@ -8,55 +8,66 @@ import { useTranslations } from 'next-intl'
 
 export function InvoiceMiningUploadFilesForm() {
 	const t = useTranslations('InvoiceMiningUploadFilesForm')
-	const [state, formAction, pending] = useActionState(invoiceMiningUploadFilesFormAction, '')
+	const [filesCount, setFilesCount] = useState(0)
+	const [jobId, formAction, pending] = useActionState(invoiceMiningUploadFilesFormAction, '')
 
-	const shouldSubmitBeDisabled = state.length > 0 || pending
+	if (jobId) {
+		return <InvoiceMiningUploadFilesFormSuccess jobId={jobId} />
+	}
 
 	return (
-		<form
-			className='my-2 flex w-full max-w-md flex-col gap-4 rounded-lg bg-white p-6 shadow-md'
-			action={formAction}
-		>
-			<div>
-				<label
-					htmlFor='files'
-					className='mb-2 block text-sm font-medium text-slate-700'
-				>
-					{t('inputFile.label')}
-				</label>
-				<input
-					id='files'
-					name='files'
-					type='file'
-					accept='.pdf'
-					multiple
-					className='file-input'
-					disabled={pending || state.length > 0}
-				/>
-			</div>
-			{state.length > 0 && (
-				<div className='alert alert-success alert-soft'>
-					<FaCheck />
-					<span>{t('successBox.message')}</span>
+		<form action={formAction}>
+			<InvoiceMiningUploadFilesFormContainer>
+				<div>
+					<label
+						htmlFor='files'
+						className='mb-2 block text-sm font-medium text-slate-700'
+					>
+						{t('inputFile.label')}
+					</label>
+					<input
+						id='files'
+						name='files'
+						type='file'
+						accept='.pdf'
+						multiple
+						className='file-input'
+						disabled={pending || jobId.length > 0}
+						onChange={e => setFilesCount(e.currentTarget.files?.length ?? 0)}
+					/>
 				</div>
-			)}
-			{state.length === 0 ? (
 				<button
 					className='btn btn-primary'
-					disabled={shouldSubmitBeDisabled}
+					disabled={filesCount === 0 || pending}
 				>
-					{shouldSubmitBeDisabled && <span className='loading loading-spinner loading-sm'></span>}
+					{pending && <span className='loading loading-spinner loading-sm'></span>}
 					{t('submit')}
 				</button>
-			) : (
-				<Link
-					href={`/invoice-mining/results/${state}`}
-					type='submit'
-					className='btn btn-success'
-				>
-					{t('viewResults')}
-				</Link>
-			)}
+			</InvoiceMiningUploadFilesFormContainer>
 		</form>
 	)
+}
+
+function InvoiceMiningUploadFilesFormSuccess({ jobId }: { jobId: string }) {
+	const t = useTranslations('InvoiceMiningUploadFilesForm')
+
+	return (
+		<InvoiceMiningUploadFilesFormContainer>
+			<div className='alert alert-success alert-soft'>
+				<FaCheck />
+				<span>{t('successBox.message')}</span>
+			</div>
+			<Link
+				href={`/invoice-mining/results/${jobId}`}
+				type='submit'
+				className='btn btn-success'
+			>
+				{t('viewResults')}
+			</Link>
+		</InvoiceMiningUploadFilesFormContainer>
+	)
+}
+
+function InvoiceMiningUploadFilesFormContainer({ children }: { children: ReactNode }) {
+	return <div className='my-2 flex w-full max-w-md flex-col gap-4 rounded-lg bg-white p-6 shadow-md'>{children}</div>
 }
