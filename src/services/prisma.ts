fe2946910e7d46c5a414e5a686518560c24a4726
Filip Balada <flpbalada@gmail.com>
2025-06-25 13:05:04 +0200

    feat: refactor Prisma client initialization to use singleton pattern

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { createSingleton } from '../utils/create-singleton'
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+export const prisma = createSingleton('prisma', () => new PrismaClient().$extends(withAccelerate()), false)
